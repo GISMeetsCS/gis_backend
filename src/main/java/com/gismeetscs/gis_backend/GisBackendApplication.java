1cb039e1b49786215159dd15fe728690f079f75f
Nayeon Kim <sksldi0@gmail.com>
2023-08-12 14:51:38 -0500

    Init Project

---
+++ src/main/java/com/gismeetscs/gis_backend/GisBackendApplication.java
@@ -0,0 +1,12 @@
+package com.gismeetscs.gis_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GisBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/gismeetscs/gis_backend/GisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gismeetscs.gis_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
